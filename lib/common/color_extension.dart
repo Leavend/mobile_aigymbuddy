eed1f32778b4a4e460ed4f0be2548cf46008834c
Tio Hady Pranoto <76149973+Leavend@users.noreply.github.com>
2025-09-30 09:21:15 +0800

    fix(ios): expose flutter module to swift pods

--- lib/common/color_extension.dart
+++ lib/common/color_extension.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
 
+/// Centralized color palette for the application.
+///
+/// Using `static const` fields avoids creating new [Color] instances every
+/// time a value is accessed and enables const expressions in widgets.
 class TColor {
-  // Primary Colors - Blue Theme (Better balanced)
-  static Color get primaryColor1 => const Color(0xff92A3FD);
-  static Color get primaryColor2 => const Color(0xff9DCEFF);
+  const TColor._();
+
+  // Primary Colors - Blue Theme (better balanced)
+  static const Color primaryColor1 = Color(0xff92A3FD);
+  static const Color primaryColor2 = Color(0xff9DCEFF);
 
   // Secondary Colors
-  static Color get secondaryColor1 => const Color(0xffC58BF2);
-  static Color get secondaryColor2 => const Color(0xffEEA4CE);
+  static const Color secondaryColor1 = Color(0xffC58BF2);
+  static const Color secondaryColor2 = Color(0xffEEA4CE);
 
   // Gradients
-  static List<Color> get primaryG => [primaryColor1, primaryColor2];
-  static List<Color> get secondaryG => [secondaryColor1, secondaryColor2];
+  static const List<Color> primaryG = [primaryColor1, primaryColor2];
+  static const List<Color> secondaryG = [secondaryColor1, secondaryColor2];
 
   // Supporting Colors
-  static Color get black => const Color(0xff1D1617);
-  static Color get yellow => const Color(0xffFFC72C);
-  static Color get white => Colors.white;
-  static Color get lightGray => const Color(0xffF7F8F8);
-  static Color get gray => const Color.fromARGB(172, 94, 111, 111);
+  static const Color black = Color(0xff1D1617);
+  static const Color yellow = Color(0xffFFC72C);
+  static const Color white = Colors.white;
+  static const Color lightGray = Color(0xffF7F8F8);
+  static const Color gray = Color.fromARGB(172, 94, 111, 111);
 }
