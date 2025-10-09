6ec8dc4b92e770014cbf06ea8c24d2f4ddaa74fc
Tio Pranoto <leavend@Tios-MacBook-Air.local>
2025-10-09 17:27:33 +0800

    refactor: minor bugs

--- lib/view/sleep_tracker/sleep_tracker_view.dart
+++ lib/view/sleep_tracker/sleep_tracker_view.dart
@@ -246,8 +246,8 @@ class _SleepTrackerViewState extends State<SleepTrackerView> {
                           ),
                         ),
                         SizedBox(
-                          width: 80,
-                          height: 30,
+                          width: 95,
+                          height: 36,
                           child: RoundButton(
                             title: "Check",
                             type: RoundButtonType.bgGradient,
